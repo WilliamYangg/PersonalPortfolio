426805380516aa53978416b48ba8bdace07cd270
William Yang <120450879+WilliamYangg@users.noreply.github.com>
2025-03-04 16:27:07 +1100

    Update Projects.tsx

--- src/Projects component/Projects.tsx
+++ src/Projects component/Projects.tsx
@@ -7,11 +7,12 @@ const Projects = () => {
       <h1>Projects</h1>
       <div className="projects-intro">Take a look at my projects below!</div>
       <div className="project-container">
-        <div className="project-picture">
-          <img src="./Placeholder.png" alt="RISKIT Screenshot" />
-        </div>
-        <div className="project-title">RISKIT - Summer 2023</div>
-        <div className="project-language">React, Typescript, CSS, Vite</div>
+        <a href="https://riskit.vercel.app/" rel="noopener noreferrer">
+          <div className="project-picture">
+            <img src="./Placeholder.png" alt="RISKIT Screenshot" />
+          </div>
+          <div className="project-title">RISKIT - Summer 2023</div>
+        </a>
         <div className="project-description">
           RISKIT is a gambling website which contains many minigames played with
           fake money, the purpose of this website was to challenge players to
